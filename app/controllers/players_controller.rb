08752016de9a4273239eb4a39f538f397b6617d3
ananevam <alexander.ananev@develonica.ru>
2015-11-27 18:18:29 +0500

    most played heroes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,5 +2,16 @@ class PlayersController < ApplicationController
   def show
     @user = User.find_by_account_id(params[:account_id])
     @matches = Match.joins(:players).where("players.account_id" => @user.account_id).order(:id=>:desc).page(params[:page])
+
+    @most_played_heroes = Player.find_by_sql(
+        ["SELECT players.*, COUNT(hero_id) as count_games, heroes.name as hero_name
+          FROM players
+          INNER JOIN heroes ON players.hero_id = heroes.id
+          WHERE account_id = (?)
+          GROUP BY hero_id
+          ORDER BY count_games DESC
+          LIMIT 10
+        ", @user.account_id]
+    )
   end
 end
