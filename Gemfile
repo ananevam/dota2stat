be3168228348c51e75066ddb7da74b11fc643543
ananevam <alexander.ananev@develonica.ru>
2015-11-30 17:38:57 +0500

    ru language

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'kaminari'
 gem 'whenever', :require => false
 gem 'dotenv-rails'
 gem 'puma'
+gem 'russian', '~> 0.6.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'matches/show'
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:sessions]
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  scope "(:locale)", locale: /en|ru/ do
+
+    devise_scope :user do
+      delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
+
+    root 'main#index'
+
+    match 'matches/:id' => 'matches#show', :as => :match, :via => [:get]
+    match 'players/:account_id' => 'players#show', :as => :player, :via => [:get]
   end
-
-  root 'main#index'
-
-  match 'matches/:id' => 'matches#show', :as => :match, :via => [:get]
-  match 'players/:account_id' => 'players#show', :as => :player, :via => [:get]
 end
